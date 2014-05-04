0aee76420391524f4df67c6c9443cd1d94583c46
Rogier <rogier777@gmail.com>
2014-05-04 11:04:23 +0200

    Use C++11 if possible
    This allows the use of more efficient (i.e. faster) data structures
    like unordered_map.

---
+++ empty.cpp
@@ -0,0 +1,6 @@
+// File used by cmake to check for c++11 support
+// See: CmakeLists.txt
+
+// Just a symbol named 'main' is required for compilation to succeed.
+// It does not have to be a function :-)
+int main;
