febd3283da540ec625af3c0939b896201bbd2b58
Sfan5 <sfan5@live.de>
2014-05-13 14:15:44 +0200

    Better error reporting for LevelDB backend

--- db-leveldb.cpp
+++ db-leveldb.cpp
@@ -24,7 +24,7 @@ DBLevelDB::DBLevelDB(const std::string &mapdir) :
 	options.create_if_missing = false;
 	leveldb::Status status = leveldb::DB::Open(options, mapdir + "map.db", &m_db);
 	if(!status.ok())
-		throw std::runtime_error("Failed to open Database");
+		throw std::runtime_error(std::string("Failed to open Database: ") + status.ToString());
 }
 
 DBLevelDB::~DBLevelDB() {
